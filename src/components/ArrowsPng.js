ba5944757ba202eac2769575ba91ac07c7e842e9
Ivan <87397558+wasivis@users.noreply.github.com>
2022-06-13 20:01:30 -0300

    Changes file names

--- src/components/ArrowsPng.js
+++ src/components/ArrowsPng.js
@@ -1,13 +1,11 @@
 import React from "react";
-import flechaderecha from "../images/flechaderecha.png";
-import flechaizquierda from "../images/flechaizquierda.png";
+import rightarrow from "../images/rightarrow.png";
+import leftarrow from "../images/leftarrow.png";
 
 export const RightArrow = () => {
-	return <img src={flechaderecha} width={60} height={60} alt="flechaderecha" />;
+	return <img src={rightarrow} width={60} height={60} alt="flechaderecha" />;
 };
 
 export const LeftArrow = () => {
-	return (
-		<img src={flechaizquierda} width={60} height={60} alt="flechaizquierda" />
-	);
+	return <img src={leftarrow} width={60} height={60} alt="flechaizquierda" />;
 };
