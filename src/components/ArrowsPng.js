c9e9383af87a282804eb135517dfde089427c7c8
Ivan <87397558+wasivis@users.noreply.github.com>
2022-06-13 20:05:07 -0300

    Changes file names and paths

--- src/components/ArrowsPng.js
+++ src/components/ArrowsPng.js
@@ -3,9 +3,9 @@ import rightarrow from "../images/rightarrow.png";
 import leftarrow from "../images/leftarrow.png";
 
 export const RightArrow = () => {
-	return <img src={rightarrow} width={60} height={60} alt="flechaderecha" />;
+	return <img src={rightarrow} width={60} height={60} alt="rightarrow" />;
 };
 
 export const LeftArrow = () => {
-	return <img src={leftarrow} width={60} height={60} alt="flechaizquierda" />;
+	return <img src={leftarrow} width={60} height={60} alt="leftarrow" />;
 };
--- src/components/LogoPng.js
+++ src/components/LogoPng.js
@@ -1,6 +1,6 @@
 import React from "react";
-import logo1 from "../images/logo1.png";
+import logo1 from "../images/logo.png";
 
 export const Logo = () => {
-	return <img src={logo1} width={200} height={100} alt="logo1" />;
+	return <img src={logo1} width={200} height={100} alt="logo" />;
 };
